5276c2e7a6b33513ae85fba3ef40d12f1d18fd5d
Ricardo Tejerina <ricardo.tejerina314@gmail.com>
2020-06-24 17:37:18 -0400

    Tarea Progra 3 Ricardo Tejerina

--- app/src/main/java/bo/upb/programacion3/codelabpokedex/PokemonDetailsActivity.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/PokemonDetailsActivity.java
@@ -6,19 +6,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class PokemonDetailsActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+
+import bo.upb.programacion3.codelabpokedex.model.Pokemon;
+
+public class PokemonDetailsActivity extends AppCompatActivity
+{
 
     private ImageView pokemonImage;
     private TextView nameTextView;
     private TextView idTextView;
     private TextView typeTextView;
 
+    private Gson gson = new Gson();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemon_details);
         initViews();
-        fillPokemonData();
+
+        Pokemon pokemon = this.gson.fromJson(getIntent().getStringExtra(Constants.POKEMON_SELECTED), Pokemon.class);
+
+        fillPokemonData(pokemon);
     }
 
     // Esto ya saben!
@@ -29,7 +39,10 @@ public class PokemonDetailsActivity extends AppCompatActivity {
         this.typeTextView = findViewById(R.id.typeTextView);
     }
 
-    private void fillPokemonData() {
-
+    private void fillPokemonData(Pokemon pokemon) {
+        this.pokemonImage.setImageResource(pokemon.getImage());
+        this.nameTextView.setText(pokemon.getName());
+        this.idTextView.setText(String.valueOf(pokemon.getId()));
+        this.typeTextView.setText(pokemon.getType());
     }
 }
--- app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
@@ -15,18 +15,43 @@ public class PokemonUtils {
      */
     public static List<Pokemon> getPokemons() {
         List<Pokemon> pokemons = new ArrayList<>();
-        pokemons.add(new Pokemon(1, "Pikachu", R.drawable.pikachu, "Electric"));
-        pokemons.add(new Pokemon(2, "Bulbasaur", R.drawable.bulbasaur, "Grass"));
-        pokemons.add(new Pokemon(3, "Charmander", R.drawable.charmander, "Fire"));
-        pokemons.add(new Pokemon(4, "Squirtle", R.drawable.squirtle, "Water"));
-        pokemons.add(new Pokemon(5, "Snorlax", R.drawable.snorlax, "Normal"));
-        pokemons.add(new Pokemon(6, "ButterFree", R.drawable.butterfree, "Bug"));
-        pokemons.add(new Pokemon(7, "Ekans", R.drawable.ekans, "Poison"));
-        pokemons.add(new Pokemon(8, "Pidgey", R.drawable.pidgey, "Normal"));
-        pokemons.add(new Pokemon(9, "Rattata", R.drawable.rattata, "Normal"));
-        pokemons.add(new Pokemon(10, "Sandshrew", R.drawable.sandshrew, "Ground"));
-        pokemons.add(new Pokemon(11, "Venonat", R.drawable.venonat, "Bug"));
-        pokemons.add(new Pokemon(12, "Zubat", R.drawable.zubat, "Poison"));
+        pokemons.add(new Pokemon(1, "Mudkip", R.drawable.mudkip, "Water"));
+        pokemons.add(new Pokemon(2, "Mega Swampert", R.drawable.mega_swampert, "Water/Ground"));
+        pokemons.add(new Pokemon(3, "Meowstic", R.drawable.meowstic, "Psychic"));
+        pokemons.add(new Pokemon(4, "Delcatty", R.drawable.delcatty, "Normal"));
+        pokemons.add(new Pokemon(5, "Delphox", R.drawable.delphox, "Fire/Psychic"));
+        pokemons.add(new Pokemon(6, "Vespiquen", R.drawable.vespiquen, "Bug/Flying"));
+        pokemons.add(new Pokemon(7, "Volcarona", R.drawable.volcarona, "Bug/Fire"));
+        pokemons.add(new Pokemon(8, "Froslass", R.drawable.froslass, "Ice/Ghost"));
+        pokemons.add(new Pokemon(9, "Pachirisu", R.drawable.pachirisu, "Electric"));
+        pokemons.add(new Pokemon(10, "Yveltal", R.drawable.yveltal, "Dark/Flying"));
+        pokemons.add(new Pokemon(11, "Pikachu", R.drawable.pikachu, "Electric"));
+        pokemons.add(new Pokemon(12, "Bulbasaur", R.drawable.bulbasaur, "Grass"));
+        pokemons.add(new Pokemon(13, "Charmander", R.drawable.charmander, "Fire"));
+        pokemons.add(new Pokemon(14, "Squirtle", R.drawable.squirtle, "Water"));
+        pokemons.add(new Pokemon(15, "Snorlax", R.drawable.snorlax, "Normal"));
+        pokemons.add(new Pokemon(16, "ButterFree", R.drawable.butterfree, "Bug"));
+        pokemons.add(new Pokemon(17, "Ekans", R.drawable.ekans, "Poison"));
+        pokemons.add(new Pokemon(18, "Pidgey", R.drawable.pidgey, "Normal"));
+        pokemons.add(new Pokemon(19, "Rattata", R.drawable.rattata, "Normal"));
+        pokemons.add(new Pokemon(20, "Sandshrew", R.drawable.sandshrew, "Ground"));
+        pokemons.add(new Pokemon(21, "Venonat", R.drawable.venonat, "Bug"));
+        pokemons.add(new Pokemon(22, "Zubat", R.drawable.zubat, "Poison"));
+        return pokemons;
+    }
+    public static List<Pokemon> getFPokemons()
+    {
+        List<Pokemon> pokemons = new ArrayList<>();
+        pokemons.add(new Pokemon(1, "Mudkip", R.drawable.mudkip, "Water"));
+        pokemons.add(new Pokemon(2, "Mega Swampert", R.drawable.mega_swampert, "Water/Ground"));
+        pokemons.add(new Pokemon(3, "Meowstic", R.drawable.meowstic, "Psychic"));
+        pokemons.add(new Pokemon(4, "Delcatty", R.drawable.delcatty, "Normal"));
+        pokemons.add(new Pokemon(5, "Delphox", R.drawable.delphox, "Fire/Psychic"));
+        pokemons.add(new Pokemon(6, "Vespiquen", R.drawable.vespiquen, "Bug/Flying"));
+        pokemons.add(new Pokemon(7, "Volcarona", R.drawable.volcarona, "Bug/Fire"));
+        pokemons.add(new Pokemon(8, "Froslass", R.drawable.froslass, "Ice/Ghost"));
+        pokemons.add(new Pokemon(9, "Pachirisu", R.drawable.pachirisu, "Electric"));
+        pokemons.add(new Pokemon(10, "Yveltal", R.drawable.yveltal, "Dark/Flying"));
         return pokemons;
     }
 }
